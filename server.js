212dafbaa38e1ccb0d7f4ea76fd5abf75a423224
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-03 21:27:16 -0400

    Setting up express for testing purposes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.json({message:'Hello World!'});
 });
 
 app.listen(3000, function () {
