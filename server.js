9806d4e4e0cbc1c13f74f783a3b22259755fa742
Sergio Rodriguez <oneandprime@gmail.com>
2016-07-03 21:23:30 -0400

    Setting up express for testing purposes

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const path = require('path')
-const express = require('express')
+var path = require('path');
+var express = require('express');
+var app = express();
 
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Pullr is listening on port 3000!');
+});
+
+/*
 module.exports = {
   app: function () {
     const app = express()
@@ -11,3 +21,4 @@ module.exports = {
     return app
   }
 }
+*/
